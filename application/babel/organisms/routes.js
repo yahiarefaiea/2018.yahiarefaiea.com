ad3ad6b715797bca0f823268116a252cd13188f4
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-08-24 17:04:28 +0300

    clean up if statments in routes.js

--- application/babel/organisms/routes.js
+++ application/babel/organisms/routes.js
@@ -3,24 +3,20 @@ Router.routes = function(callback) {
   var location = Router.location.split('/').filter(Boolean)
 
   //  HOME
-  if(location[0] === undefined) {
+  if(location[0] === undefined)
     Router.push('home')
-  }
 
   //  SECRET
-  else if(location[0] == 'secret' && location[1] === undefined) {
+  else if(location[0] == 'secret' && location[1] === undefined)
     Router.push('secret')
-  }
 
   //  BUCKET
-  else if(location[0] == 'bucket' && location[1] === undefined) {
+  else if(location[0] == 'bucket' && location[1] === undefined)
     Router.push('bucket')
-  }
 
   //  BUCKET ALL
-  else if(location[0] == 'bucket' && location[1] == 'all' && location[2] === undefined) {
+  else if(location[0] == 'bucket' && location[1] == 'all' && location[2] === undefined)
     Router.push('bucketAll')
-  }
 
   //  NOT FOUND
   else Router.push('notFound')
