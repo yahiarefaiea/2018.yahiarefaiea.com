770ae9194bced29bbe8235b0e255e1162e5fe33f
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-09-03 20:35:56 +0300

    finalizing input.js

--- application/babel/molecules/input.js
+++ application/babel/molecules/input.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
   var input = $('.field').find('input, textarea')
 
   input.keyup(function() {
-    var length = $.trim($(this).val()).length
     var field = $(this).closest('.field')
     var form = $(this).closest('form, .form')
+    var length = $.trim($(this).val()).length
 
     //  FILLED
     if(length > 0) field.addClass('filled')
