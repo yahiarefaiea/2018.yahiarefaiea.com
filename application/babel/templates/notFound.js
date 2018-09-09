3ac198c39fc0747caf0612be21905db905a3ffb5
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-09-09 03:33:12 +0300

    added notFound.js

---
+++ application/babel/templates/notFound.js
@@ -0,0 +1,8 @@
+function notFound() {
+  setTimeout(function() {
+    Timer.run('.template[data-template="notFound"] time',
+    function() {
+      Router.route('#')
+    }, 5)
+  }, Identity.duration*1.25)
+}
