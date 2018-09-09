189a9bec25799e6ec30a0e585712401ec6a08da4
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-09-09 17:14:54 +0300

    update secret.js

--- application/babel/templates/secret.js
+++ application/babel/templates/secret.js
@@ -1,12 +1,8 @@
 function secret() {
-  var input = $('.template[data-template=secret] .field').find('input, textarea')
-
-  //  FOCUS
   setTimeout(function() {
-    input.focus()
-  }, Identity.duration*1.25)
+    var input = $('.template[data-template=secret] .field').find('input, textarea')
 
-  //  ROBOT
-  input.focus(Identity.robot)
-  input.focusout(Identity.stop)
+    input.focus()
+    Identity.robot()
+  }, Identity.duration*1.25)
 }
