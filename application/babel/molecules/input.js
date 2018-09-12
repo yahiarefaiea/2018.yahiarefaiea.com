7d6d9052e00c67a00a0cd41ae92d7ebc6f512dab
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-09-12 14:33:45 +0300

    split input test to a function

--- application/babel/molecules/input.js
+++ application/babel/molecules/input.js
@@ -1,24 +1,25 @@
 $(document).ready(function() {
-
   var input = $('.field').find('input, textarea')
-
   input.keyup(function() {
-    var field = $(this).closest('.field')
-    var form = $(this).closest('form, .form')
-    var length = $.trim($(this).val()).length
-
-    //  FILLED
-    if(length > 0) field.addClass('filled')
-    else field.removeClass('filled')
-
-    //  VALIDATED
-    if(length > 5) {
-      field.addClass('validated')
-      form.addClass('validated')
-    } else {
-      field.removeClass('validated')
-      form.removeClass('validated')
-    }
+    inputTest(this)
   })
-
 })
+
+function inputTest(that) {
+  var field = $(that).closest('.field')
+  var form = $(that).closest('form, .form')
+  var length = $.trim($(that).val()).length
+
+  //  FILLED
+  if(length > 0) field.addClass('filled')
+  else field.removeClass('filled')
+
+  //  VALIDATED
+  if(length > 5) {
+    field.addClass('validated')
+    form.addClass('validated')
+  } else {
+    field.removeClass('validated')
+    form.removeClass('validated')
+  }
+}
