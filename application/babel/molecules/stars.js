faca966d6886a5adc890e790728b82dd71231449
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-09-14 18:19:24 +0300

    added stars.js and yahia.js canvas

---
+++ application/babel/molecules/stars.js
@@ -0,0 +1,119 @@
+var Stars = {
+  canvas: null,
+  context: null,
+  circleArray: [],
+  colorArray: [
+    '#4c1a22',
+    '#4c1a23',
+    '#5d6268',
+    '#1f2e37',
+    '#474848',
+    '#542619',
+    '#ead8cf',
+    '#4c241f',
+    '#d6b9b1',
+    '#964a47'
+  ],
+
+  mouseDistance: 50,
+  radius: .5,
+  maxRadius: 1.5,
+
+  //  MOUSE
+  mouse: {
+    x: undefined,
+    y: undefined,
+    down: false,
+    move: false
+  },
+
+  //  INIT
+  init: function() {
+    this.canvas = document.getElementById('stars')
+    this.canvas.width = window.innerWidth
+    this.canvas.height = window.innerHeight
+    this.canvas.style.display = 'block'
+    this.context = this.canvas.getContext('2d')
+
+    window.addEventListener('mousemove', this.mouseMove)
+    window.addEventListener('resize', this.resize)
+
+    this.prepare()
+    this.animate()
+  },
+
+  //  CIRCLE
+  Circle: function(x, y, dx, dy, radius, fill) {
+    this.x = x
+    this.y = y
+    this.dx = dx
+    this.dy = dy
+    this.radius = radius
+    this.minRadius = this.radius
+
+    this.draw = function() {
+      Stars.context.beginPath()
+      Stars.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+      Stars.context.fillStyle = fill
+      Stars.context.fill()
+    }
+
+    this.update = function() {
+      if(this.x + this.radius > Stars.canvas.width || this.x - this.radius < 0) this.dx = -this.dx
+      if(this.y + this.radius > Stars.canvas.height || this.y - this.radius < 0) this.dy = -this.dy
+
+      this.x += this.dx
+      this.y += this.dy
+
+      //  INTERACTIVITY
+      if(Stars.mouse.x - this.x < Stars.mouseDistance && Stars.mouse.x - this.x > -Stars.mouseDistance
+        && Stars.mouse.y - this.y < Stars.mouseDistance && Stars.mouse.y - this.y > -Stars.mouseDistance) {
+        if(this.radius < Stars.maxRadius) this.radius +=1
+      } else if(this.radius > this.minRadius) {
+        this.radius -= 1
+      }
+
+      this.draw()
+    }
+  },
+
+  //  PREPARE
+  prepare: function() {
+    this.circleArray = []
+
+    for(var i = 0; i < 1200; i++) {
+      var radius = Stars.radius
+      var x = Math.random() * (this.canvas.width - radius * 2) + radius
+      var y = Math.random() * (this.canvas.height - radius * 2) + radius
+      var dx = (Math.random() - 0.5) * 2
+      var dy = (Math.random() - 1) * 2
+      var fill = this.colorArray[Math.floor(Math.random() * this.colorArray.length)]
+
+      this.circleArray.push(new this.Circle(x, y, dx, dy, radius, fill))
+    }
+  },
+
+  //  ANIMATE
+  animate: function() {
+    requestAnimationFrame(Stars.animate)
+    Stars.context.clearRect(0, 0, Stars.canvas.width, Stars.canvas.height)
+
+    for(var i = 0; i < Stars.circleArray.length; i++) {
+      var circle = Stars.circleArray[i]
+      circle.update()
+    }
+  },
+
+  //  MOUSE MOVE
+  mouseMove: function(event) {
+    Stars.mouse.x = event.x
+    Stars.mouse.y = event.y
+  },
+
+  //  RESIZE
+  resize: function() {
+    this.canvas.width = window.innerWidth
+    this.canvas.height = window.innerHeight
+    init()
+  }
+}
---
+++ application/babel/molecules/yahia.js
@@ -0,0 +1,130 @@
+var renderer, scene, camera, ww, wh, particles;
+
+ww = window.innerWidth,
+wh = window.innerHeight;
+
+var centerVector = new THREE.Vector3(0, 0, 0);
+var previousTime = 0
+	speed = 10
+	isMouseDown = false;
+
+var getImageData = function(image) {
+
+	var canvas = document.createElement("canvas");
+	canvas.width = image.width;
+	canvas.height = image.height;
+
+	var ctx = canvas.getContext("2d");
+	ctx.drawImage(image, 0, 0);
+
+	return ctx.getImageData(0, 0, image.width, image.height);
+}
+
+function getPixel( imagedata, x, y ) {
+  var position = ( x + imagedata.width * y ) * 4, data = imagedata.data;
+  return { r: data[ position ], g: data[ position + 1 ], b: data[ position + 2 ], a: data[ position + 3 ] };
+}
+
+var drawTheMap = function() {
+
+	var geometry = new THREE.Geometry();
+	var material = new THREE.PointCloudMaterial();
+	material.vertexColors = true;
+	material.transparent = true;
+	for (var y = 0, y2 = imagedata.height; y < y2; y += 1) {
+		for (var x = 0, x2 = imagedata.width; x < x2; x += 1) {
+			if (imagedata.data[(x * 4 + y * 4 * imagedata.width)] > 0) {
+
+				var vertex = new THREE.Vector3();
+				vertex.x = x - imagedata.width / 2 + (500 - 440*.5);
+				vertex.y = -y + imagedata.height / 2;
+				vertex.z = -Math.random()*500;
+
+				vertex.speed = Math.random() / speed + 0.015;
+
+				var pixelColor = getPixel( imagedata, x, y )
+				var color = "rgb("+pixelColor.r+", "+pixelColor.g+", "+pixelColor.b+")"
+				geometry.colors.push(new THREE.Color(color));
+				geometry.vertices.push(vertex);
+			}
+		}
+	}
+	particles = new THREE.Points(geometry, material);
+
+	scene.add(particles);
+
+	requestAnimationFrame(render);
+};
+
+var init = function() {
+	renderer = new THREE.WebGLRenderer({
+		canvas: document.getElementById("yahia"),
+		antialias: true,
+		alpha: true
+	});
+	renderer.setSize(ww, wh);
+
+	scene = new THREE.Scene();
+
+	camera = new THREE.OrthographicCamera( ww / - 2, ww / 2, wh / 2, wh / - 2, 1, 1000 );
+	camera.position.set(0, -20, 4);
+	camera.lookAt(centerVector);
+	scene.add(camera);
+	camera.zoom = 1;
+	camera.updateProjectionMatrix();
+
+	imagedata = getImageData(image);
+	drawTheMap();
+
+    window.addEventListener('mousemove', onMousemove, false);
+    window.addEventListener('mousedown', onMousedown, false);
+    window.addEventListener('mouseup', onMouseup, false);
+    window.addEventListener('resize', onResize, false);
+
+};
+var onResize = function(){
+	ww = window.innerWidth;
+	wh = window.innerHeight;
+	renderer.setSize(ww, wh);
+    camera.left    = ww / - 2;
+    camera.right   = ww / 2;
+    camera.top     = wh / 2;
+    camera.bottom  = wh / - 2;
+    camera.updateProjectionMatrix();
+};
+
+var onMouseup = function(){
+	isMouseDown = false;
+}
+var onMousedown = function(e){
+	isMouseDown = true;
+	lastMousePos = {x:e.clientX, y:e.clientY};
+};
+var onMousemove = function(e){
+	if(isMouseDown){
+		camera.position.x += (e.clientX-lastMousePos.x)/100;
+		camera.position.y -= (e.clientY-lastMousePos.y)/100;
+		camera.lookAt(centerVector);
+		lastMousePos = {x:e.clientX, y:e.clientY};
+	}
+};
+
+var render = function(a) {
+
+	requestAnimationFrame(render);
+
+
+	particles.geometry.verticesNeedUpdate = true;
+	if(!isMouseDown){
+		camera.position.x += (0-camera.position.x)*0.06;
+		camera.position.y += (0-camera.position.y)*0.06;
+		camera.lookAt(centerVector);
+	}
+
+	renderer.render(scene, camera);
+};
+
+var imgData = 'includes/images/yahiarefaiea.png';
+
+var image = document.createElement("img");
+image.src = imgData;
