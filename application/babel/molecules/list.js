2550e678000ffd4615207705b16c8417ea008b52
Yahia Refaiea <YahiaRefaiea@gmail.com>
2019-02-24 02:20:50 +0200

    list.js is now by it's own

---
+++ application/babel/molecules/list.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  var list = $('.list')
+  var link = list.find('li.listItem a, li.archived a')
+
+  //  LISTEN
+  link.hover(function() {
+    this.closest('.list').addClass('hover')
+  }, function() {
+    this.closest('.list').removeClass('hover')
+  })
+})
